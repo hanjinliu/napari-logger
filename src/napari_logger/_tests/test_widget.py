9f2245d415ebb55df7f78fbf8ae8760dac561b55
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-01-28 10:55:24 +0900

    add more tests

--- src/napari_logger/_tests/test_widget.py
+++ src/napari_logger/_tests/test_widget.py
@@ -26,3 +26,29 @@ def test_html():
     naplogger.checkboxes.html.value = True
     print("<b>0</b>")
     assert naplogger.logger.value == "0\n\n"
+
+
+def test_logging():
+    import logging
+
+    logger = logging.getLogger(__name__)
+    naplogger = NapariLogger()
+    rh_formatter = logging.Formatter("%(levelname)s|| %(message)s")
+    naplogger.logger.setFormatter(rh_formatter)
+
+    logger.warning("0")
+    assert naplogger.logger.value == ""
+
+    naplogger.checkboxes.logging.value = True
+    logger.warning("0")
+    assert naplogger.logger.value == "WARNING|| 0\n"
+
+
+def test_matplotlib():
+    import matplotlib.pyplot as plt
+
+    naplogger = NapariLogger()
+    naplogger.checkboxes.plotting.value = True
+
+    plt.plot([0, 1, 2])
+    plt.show()
